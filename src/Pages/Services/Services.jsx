e19c292198dfcf0af211d68dede9a5204665478c
RitaM5 <sporshia108514orva@gmail.com>
2022-12-17 02:00:36 +0600

    slick slider added in service

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -3,6 +3,9 @@ import { Container } from 'react-bootstrap'
 import HomeServices from '../../../public/Utilites/HomeServices'
 import './Services.css'
 import Testimonial from './Testimonial'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Services = () => {
   const [testimonial, setTestimonial] = useState([]);
   useEffect(() => {
@@ -10,21 +13,61 @@ const Services = () => {
       .then(res => res.json())
       .then(data => setTestimonial(data))
   }, [])
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <div className='py-5'>
       <HomeServices />
-      <Container>
+      <Container className=''>
         <div className="text-center">
           <h2 className='fw-bold'>Testimonials</h2>
           <div className='section-header my-3'></div>
           <p>Voluptatem quibusdam ut ullam perferendis repellat non ut consequuntur est eveniet deleniti fignissimos eos quam</p>
         </div>
-        <div className="row py-4 mt-5">
-          {
-            testimonial.map(testData => <Testimonial testData={testData} key={testimonial.id}/>)
-          }
-        </div>
       </Container>
+      <section className='silks py-5 mt-5'>
+        <Container>
+          <div className="p-4 py-5">
+            <Slider {...settings}>
+              {
+                testimonial.map(testData => <Testimonial testData={testData} key={testimonial.id} />)
+              }
+            </Slider>
+          </div>
+        </Container>
+      </section>
     </div>
   )
 }
--- src/Pages/Services/Testimonial.jsx
+++ src/Pages/Services/Testimonial.jsx
@@ -4,11 +4,11 @@ const Testimonial = (props) => {
     const { id, name, title, details, img } = props.testData;
     return (
         <>
-            <div className="col-lg-4 gy-2">
-                <div className="card h-100 border-0" style={{ backgroundColor:'yellowgreen' }}>
+            <div className=''>
+                <div className="card h-100 border-0  silks-slide" style={{ backgroundColor: 'yellowgreen' }}>
                     <div className="card-body text-center">
                         <div className='d-flex gap-3 align-items-center'>
-                            <img src={img} style={{width:65, height:65}} className="rounded-5" alt="" />
+                            <img src={img} style={{ width: 65, height: 65 }} className="rounded-5" alt="" />
                             <div className='text-start'>
                                 <h5 className="fw-bold text-white pt-2">{name}</h5>
                                 <span className="">{title}</span>
@@ -23,3 +23,26 @@ const Testimonial = (props) => {
 }
 
 export default Testimonial
+/* 
+         <div className="row">
+  
+
+
+
+                         <div className='card'>
+                    <div className='card-top'>
+                        <div className='d-flex gap-3 align-items-center'>
+                            <img src={img} style={{ width: 65, height: 65 }} className="rounded-5" alt="" />
+                            <div className='text-start'>
+                                <h5 className="fw-bold text-white pt-2">{name}</h5>
+                                <span className="">{title}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card-bottom'>
+                    <p className='pt-4 text-start'>{details}</p>
+                    </div>
+                </div>
+        
+
+*/
